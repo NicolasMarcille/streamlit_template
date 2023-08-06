9c3aa624acfd806525f6b8d38402bccd180fbe5f
nmarcille <nmarcille@ea.com>
2023-08-06 15:41:19 +0300

    first commit from local

---
+++ Home.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import logging
+import os
+import sys
+
+
+st.title("Nick's app")
+
+st.sidebar.markdown("## Home page 🎈")
+st.subheader("Here is a subheader")
+
+
+text = '''
+#### This is my app template.
+'''
+text
+
+
+x = st.slider('My slider')
+st.write(x, 'squared is', x * x)
+
+st.text_input("My text input", key="text_input")
+st.session_state.text_input
+
+if st.checkbox('Show Me'):
+    test_data = "Some text hidden that can be shown"
+    test_data
+
+option = st.selectbox('Which letter do you prefer?', options=['a', 'b', 'c', 'd'])
+
+'You selected: ', option
+
+select_box = st.sidebar.selectbox('How would you like to be contacted?',
+                                  options=('Email', 'Home phone', 'Mobile phone'))
+
+
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('Press me!')
+
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio('Sorting hat', options=("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
+
+
+
+
+
+
+
---
+++ pages/About.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.write("## Here is the About page!")
+st.sidebar.markdown("## About page")
+
