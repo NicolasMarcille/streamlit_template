c751d4177b95b0c9e4f52db2b551a2e299487690
nmarcille <nmarcille@ea.com>
2023-08-14 00:27:53 +0300

    eod

---
+++ Home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.title("Nick's language identification app")
+
+st.subheader("Choose the relevant page in the side menu")
---
+++ pages/03_From_Recording.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from lib.st_custom_components import st_audiorec
+
+st.title("What's that language?")
+
+wav_audio_data = st_audiorec()
+
+
+if wav_audio_data is not None:
+    # display audio data as received on the backend
+    st.audio(wav_audio_data, format='audio/wav')
+
+# INFO: by calling the function an instance of the audio recorder is created
+# INFO: once a recording is completed, audio data will be saved to wav_audio_data
+
+
+
+
