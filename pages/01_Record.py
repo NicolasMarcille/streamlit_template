a9d815bda5d09cc167211ac03607629ef5aa1f12
Marcille, Nicolas <nmarcille@ea.com>
2024-05-27 23:07:50 +0200

    new version with mic package

--- pages/01_Record.py
+++ pages/01_Record.py
@@ -2,6 +2,7 @@ import streamlit as st
 from lib.whisper_stt import whisper_stt
 from lib.utils import find_language, translate
 
+
 text = whisper_stt(language='en')
 # If you don't pass an API key, the function will attempt to retrieve it as an environment variable : 'OPENAI_API_KEY'.
 
---
+++ pages/02_Old_component.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from st_custom_components import st_audiorec
+from lib import utils
+
+
+if 'language' not in st.session_state:
+    st.session_state.language = None
+
+if 'transcript' not in st.session_state:
+    st.session_state.transcript = None
+
+st.markdown("#### Record voice and submit:")
+
+with st.form('form_audio'):
+    wav_audio_data = st_audiorec()
+    if wav_audio_data is not None:
+        with open('temp_file.wav', 'wb') as fp:
+            fp.write(wav_audio_data)
+    if st.form_submit_button('Submit this'):
+        st.session_state.transcript = utils.transcribe_audio('temp_file.wav')
+        st.session_state.language = utils.find_language(st.session_state.transcript)
+
+
+if st.session_state.language is not None:
+    st.markdown("##### The language is:")
+    st.markdown(f"### {st.session_state.language}")
+
+    st.markdown("##### The translation in English is:")
+    st.markdown(f"### {utils.translate(st.session_state.transcript)}")
+
