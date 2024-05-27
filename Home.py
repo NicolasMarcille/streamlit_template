345bad93b5f3dedc9bf96a775da62a78023a2486
Marcille, Nicolas <nmarcille@ea.com>
2024-05-27 23:47:56 +0200

    try mobile version

--- Home.py
+++ Home.py
@@ -1,8 +1,29 @@
 import streamlit as st
-from st_custom_components import st_audiorec
-from lib import utils
+from lib.whisper_stt import whisper_stt
+from lib.utils import find_language, translate
 from streamlit_extras.switch_page_button import switch_page
 
 # openai.api_key = st.secrets['OPENAI_API_KEY']
 
 st.title("Audio peep")
+
+
+if 'transcript' not in st.session_state:
+    st.session_state.transcript = None
+
+text = whisper_stt(start_prompt="Start",
+                   stop_prompt="Stop",
+                   use_container_width=True)
+# If you don't pass an API key, the function will attempt to retrieve it as an environment variable : 'OPENAI_API_KEY'.
+
+if text:
+    st.session_state.transcript = text
+
+    st.markdown("##### The language is:")
+    st.markdown(f"### {find_language(text)}")
+
+    st.markdown("##### The translation in English is:")
+    st.markdown(f"### {translate(text)}")
+
+
+
