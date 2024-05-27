0c2dbba6351ea8154ab12aa0a0704a5190771ee8
Marcille, Nicolas <nmarcille@ea.com>
2024-05-27 23:41:25 +0200

    try mobile version

--- pages/01_Record.py
+++ pages/01_Record.py
@@ -6,18 +6,17 @@ from lib.utils import find_language, translate
 if 'transcript' not in st.session_state:
     st.session_state.transcript = None
 
-text = whisper_stt(language='en')
+text = whisper_stt(start_prompt="Start",
+                   stop_prompt="Stop",
+                   use_container_width=True)
 # If you don't pass an API key, the function will attempt to retrieve it as an environment variable : 'OPENAI_API_KEY'.
 
 if text:
     st.session_state.transcript = text
-    st.write(text)
 
-if st.session_state.transcript is not None:
     st.markdown("##### The language is:")
     st.markdown(f"### {find_language(text)}")
 
     st.markdown("##### The translation in English is:")
     st.markdown(f"### {translate(text)}")
-
 
