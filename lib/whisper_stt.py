b9eae6655126b28121150b2e0e79f03cec54f802
Marcille, Nicolas <nmarcille@ea.com>
2024-06-20 10:32:33 +0200

    eod

--- lib/whisper_stt.py
+++ lib/whisper_stt.py
@@ -6,7 +6,7 @@ import os
 
 
 def whisper_stt(openai_api_key=None, start_prompt="Start recording", stop_prompt="Stop recording", just_once=False,
-               use_container_width=False, language=None, callback=None, args=(), kwargs=None, key=None):
+                use_container_width=False, language=None, callback=None, args=(), kwargs=None, key=None):
     if not 'openai_client' in st.session_state:
         st.session_state.openai_client = OpenAI(api_key=openai_api_key or os.getenv('OPENAI_API_KEY'))
     if not '_last_speech_to_text_transcript_id' in st.session_state:
